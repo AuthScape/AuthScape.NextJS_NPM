71149fb512b6fbd007117503161ef722ef06c9b3
Brandon Zuech <Brandonzuech@hotmail.com>
2025-08-15 15:41:40 -0400

    ver fix

--- authscape-marketplace/src/marketplace.js
+++ authscape-marketplace/src/marketplace.js
@@ -291,7 +291,7 @@ const Marketplace = ({
                       marginLeft: -1,
                     }}
                   >
-                    {category.category == "Manufacturer" ? "Brand" : category.category}
+                    {category.category}
                   </AccordionSummary>
                   <Box
                     sx={{
